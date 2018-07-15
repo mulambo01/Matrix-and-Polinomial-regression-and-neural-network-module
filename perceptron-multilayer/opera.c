6beb74976be8cf07b0b83abebc720f6ec8f19bc5
Pixote <mulambo01@protonmail.com>
2018-07-15 20:19:05 -0300

    atualiza

--- perceptron-multilayer/opera.c
+++ perceptron-multilayer/opera.c
@@ -12,8 +12,6 @@ void main(){
  qtneurons[0]=10;
  qtneurons[1]=3;
  w=loadnet("layers", "layer", qtneurons, qtlayers, qtinput-1);
-draw(w[0][3]);
-draw(w[1][1]);
 
  input=nullmatrix(1,qtinput);
  input.data[0][0]=-1.0;
@@ -21,9 +19,9 @@ draw(w[1][1]);
   scanf("%Lf", &input.data[0][i]);
  }
 
-mtx d;
-d=nullmatrix(1,3);
+ mtx d;
+ d=nullmatrix(1,qtneurons[qtlayers-1]);
 
- y=netthink(input, w, qtneurons, qtlayers, ftype);
+ y=netansw(input, w, qtneurons, qtlayers, ftype);
  draw(y);
 }
