c2dc1c87ba4daf629cc957276bbb64ff0091ed8a
Pixote <mulambo01@protonmail.com>
2018-07-24 18:34:29 -0300

    add

---
+++ modules/matrix.h
@@ -0,0 +1,28 @@
+typedef struct
+{
+ int nrows;
+ int ncols;
+ long double **data;
+}mtx;
+
+mtx crystalmatrix(int nrows, int ncols, long double value);
+mtx nullmatrix(int nrows, int ncols);
+mtx randmatrix(int nrows, int ncols, unsigned int seed);
+void draw(mtx matrix);
+mtx mtxload(char *filename, int nrows, int ncols);
+void mtxsave(char *filename, mtx matrix);
+mtx mtxsum(mtx matrix1, mtx matrix2);
+mtx mtxsub(mtx matrix1, mtx matrix2);
+mtx mtxtermsmult(mtx matrix1, mtx matrix2);
+mtx mtxmult(mtx matrix, long double q);
+mtx mtxprod(mtx matrix1, mtx matrix2);
+mtx transpose(mtx matrix);
+mtx mtxclone(mtx A);
+mtx mtxcut(mtx matrix, int srowcut, int nrows, int scolumncut, int ncols);
+long double vectprod(mtx vec1, mtx vec2);
+void mtxcopy(mtx *matrix1, mtx matrix2);
+void mtxfree(mtx *matrix);
+void putline(mtx *matrix, mtx line, int posi);
+void addline(mtx *matrix, mtx line, int posi);
+void putcol(mtx *matrix, mtx col, int posi);
+void addcol(mtx *matrix, mtx col, int posi);
---
+++ modules/pmc.h
@@ -0,0 +1,25 @@
+#include "matrix.h"
+
+#define SIGM 0
+#define TGHYP 1
+#define LINEAR 2
+
+long double sigm(long double u);
+long double sigm2(long double u);
+long double func(long double u, int ftype);
+long double func2(long double u, int ftype);
+mtx **createneurons(int *qtneurons, int qtlayers, int qtinput, unsigned int seed);
+long double neuronthink(mtx x, mtx w);
+long double neuronansw(long double input, int ftype);
+mtx layerthink(mtx x, mtx *w, int qtneurons);
+mtx layeransw(mtx input, int ftype);
+mtx netthink(mtx x, mtx **w, int *qtneurons, int qtlayers, int *ftype);
+mtx netansw(mtx x, mtx **w, int *qtneurons, int qtlayers, int *ftype);
+void fitbydelta(mtx x, mtx **w, int qtlayers, mtx d, mtx *input, mtx *y, long double lrn, int *ftype);
+mtx adjust(mtx x, mtx **w, int *qtneurons, int qtlayers, mtx d, long double lrn, int *ftype);
+long double meansqrerr(mtx samples, mtx **w, int *qtneurons, int qtlayers, mtx d, int *ftype);
+void savelayer(char *filename, mtx *w, int qtneurons);
+void savenet(char *dirname, char *fileprefix, mtx **w, int *qtneurons, int qtlayers);
+mtx* loadlayer(char *filename, int qtneurons, int qtw);
+mtx** loadnet(char *dirname, char *fileprefix, int *qtneurons, int qtlayers, int qtinput);
+
